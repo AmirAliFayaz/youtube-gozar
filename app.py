aeab140c3899b6dffb2d14edcbdd5509889eedd0
K4sra <96987353+itzk4sra@users.noreply.github.com>
2024-11-09 06:03:23 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import threading
+import tkinter as tk
 import customtkinter as ctk
+import socket
 import proxy
 import smlwb
+
 
 def toggle_connection():
     global connected
@@ -10,43 +13,51 @@ def toggle_connection():
         proxy_address = '127.0.0.1'
         proxy_port = '4525'
         isOk = proxy.set_proxy(proxy_address, proxy_port)
+        print(15, isOk)
         if not isOk:
             return
-        threading.Thread(target=run_app).start()
-        connect_button.configure(text="Disconnect", fg_color="blue")
+        threading.Thread(target=run_app, daemon=True).start()  
+        update_button("Disconnect", "white", "green")
     else:
         isOk = proxy.unset_proxy()
+        print(22, isOk)
         if not isOk:
             return
-        connect_button.configure(text="Connect", fg_color="SystemButtonFace")
+        update_button("Connect", "black", "SystemButtonFace")
+
 
 def run_app():
     smlwb.runApp()
 
+
 def close_app():
     smlwb.stop()
+
+
+def update_button(text, text_color, fg_color):
+    connect_button.configure(text=text, text_color=text_color, fg_color=fg_color)
+
 
 def on_closing():
     if connected:
         proxy.unset_proxy()
         close_app()
-    root.destroy()
     root.quit()
-    exit(0)
+    root.destroy()
+
 
 connected = False
 
-ctk.set_appearance_mode("light")
-ctk.set_default_color_theme("blue")
-
 root = ctk.CTk()
 root.title("Youtube Gozar")
-root.geometry("300x150")
+root.wm_title("Youtube Gozar v1.0")
+root.geometry("300x200")
 root.resizable(0, 0)
+
 root.protocol("WM_DELETE_WINDOW", on_closing)
 root.bind('<Escape>', lambda e: root.quit())
 
-connect_button = ctk.CTkButton(root, text="Connect", command=toggle_connection, fg_color="blue", hover_color="darkblue")
+connect_button = ctk.CTkButton(root, text="Connect", command=toggle_connection)
 connect_button.pack(pady=20)
 
 root.mainloop()
